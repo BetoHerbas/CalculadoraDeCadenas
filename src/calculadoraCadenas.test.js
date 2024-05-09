7d9a8d0c27322e16d7e0f422607cd616fec6233a
BetoHerbas <betitoherbas@gmail.com>
2024-05-09 09:55:23 -0400

    devuelve 3 como total de la suma cuando la cadena es '3'

--- src/calculadoraCadenas.test.js
+++ src/calculadoraCadenas.test.js
@@ -4,4 +4,7 @@ describe("Sumar cadena", () => {
   it("devuelve 0 como total de la suma cuando la cadena es vacia", () => {
     expect(sumarCadena("")).toEqual(0);
   });
+  it("devuelve 3 como total de la suma cuando la cadena es '3'", () => {
+    expect(sumarCadena("3")).toEqual(3);
+  });
 });
