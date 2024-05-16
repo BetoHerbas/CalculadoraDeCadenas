a6af1e10bb6a1e39ca872ab2de71cf6f5bb43bd3
Beto Herbas <betitoherbas@gmail.com>
2024-05-15 22:24:56 -0400

    refactoring: cambiando nombres de variables y funcion

--- src/calculadoraCadenas.js
+++ src/calculadoraCadenas.js
@@ -1,31 +1,31 @@
-export default function sumarCadena(cadena) {
-  let delimiters = [',', '-', /[,|-]/];
-  let numbers = cadena;
-  let acc = 0;
+export default function addChain(inputString) {
+  let defaultDelimiters = [',', '-', /[,|-]/];
+  let numbersArray = inputString;
+  let sum = 0;
 
-  const delimiterMatch = cadena.match(/^\/\/\[(.)\]/);
+  const customDelimiterMatch = inputString.match(/^\/\/\[(.)\]/);
 
-  if(delimiterMatch){
-    const delimiter = delimiterMatch[1];
-    delimiters.pop();
-    delimiters.push(new RegExp(`[${delimiter}|,|-]`));
-    numbers = cadena.slice(delimiterMatch[0].length +3 );
+  if(customDelimiterMatch){
+    const delimiter = customDelimiterMatch[1];
+    defaultDelimiters.pop();
+    defaultDelimiters.push(new RegExp(`[${delimiter}|,|-]`));
+    numbersArray = inputString.slice(customDelimiterMatch[0].length +3 );
   }
 
-  numbers = cadena.split(delimiters[2]);
+  numbersArray = inputString.split(defaultDelimiters[2]);
 
-  if(cadena === "")
+  if(inputString === "")
     return 0;
 
-  if(numbers.length == 1)
-    return Number(numbers[0]);
+  if(numbersArray.length == 1)
+    return Number(numbersArray[0]);
 
   else{  
 
-    for(const num of numbers){
-      if(num <= 1000)
-        acc = acc + Number(num);
+    for(const number of numbersArray){
+      if(number <= 1000)
+        sum = sum + Number(number);
     }    
-    return acc;
+    return sum;
   }
 }
--- src/calculadoraCadenas.test.js
+++ src/calculadoraCadenas.test.js
@@ -1,22 +1,22 @@
-import sumarCadena from "./calculadoraCadenas.js";
+import addChain from "./calculadoraCadenas.js";
 
 describe("Sumar cadena", () => {
   it("devuelve 0 como total de la suma cuando la cadena es vacia", () => {
-    expect(sumarCadena("")).toEqual(0);
+    expect(addChain("")).toEqual(0);
   });
   it("devuelve 3 como total de la suma cuando la cadena es '3'", () => {
-    expect(sumarCadena("3")).toEqual(3);
+    expect(addChain("3")).toEqual(3);
   });
   it("devuelve el total de la suma de 2 números separados por ','", () => {
-    expect(sumarCadena("1,2")).toEqual(3);
+    expect(addChain("1,2")).toEqual(3);
   });
   it("devuelve el total de la suma de varios números separados por ','", () => {
-    expect(sumarCadena("1,2,3,4,5")).toEqual(15);
+    expect(addChain("1,2,3,4,5")).toEqual(15);
   });
   it("devuelve el total de la suma de varios números separados por ',' y '-'", () => {
-    expect(sumarCadena("1-2,3")).toEqual(6);
+    expect(addChain("1-2,3")).toEqual(6);
   });
   it("devuelve el total de la suma de varios números tomando en cuenta el separador definido por el usuario", () => {
-    expect(sumarCadena("//[;],6,3;2,4")).toEqual(15);
+    expect(addChain("//[;],6,3;2,4")).toEqual(15);
   });
 });
